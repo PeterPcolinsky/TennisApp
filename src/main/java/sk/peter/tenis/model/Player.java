a223a96c3f97ca64b9e2775ee3c0e679d01f9597
PeterPcolinsky <pcolinsky.peter@centrum.sk>
2026-02-23 11:46:17 +0100

    refactor: implement equals and hashCode in Player model

--- src/main/java/sk/peter/tenis/model/Player.java
+++ src/main/java/sk/peter/tenis/model/Player.java
@@ -1,5 +1,7 @@
 package sk.peter.tenis.model;
 
+import java.util.Objects;
+
 public class Player {
     private String name;
     private int age;
@@ -8,7 +10,6 @@ public class Player {
     /**
      * Doménový objekt hráča: meno, vek a typ (amatér/profesionál).
      */
-
     public Player(String name, int age, PlayerType type) {
         this.name = name;
         this.age = age;
@@ -30,5 +31,20 @@ public class Player {
     @Override
     public String toString() {
         return "Hráč: " + name + " (" + age + " r.) - " + type.getDisplayName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return age == player.age &&
+                Objects.equals(name, player.name) &&
+                type == player.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, type);
     }
 }
